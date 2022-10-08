7d99fe59d1e26a5fa35ef654d32b05f771b52f60
zd <1836650216@qq.com>
2022-10-08 09:27:11 +0800

    idea first1

--- git-test/src/main/java/com/ztzz/git/GitTest.java
+++ git-test/src/main/java/com/ztzz/git/GitTest.java
@@ -7,5 +7,6 @@ package com.ztzz.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
