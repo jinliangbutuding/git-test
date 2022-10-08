c4eee8aac2585484564ee6925d4ae5bc20c4453e
zd <1836650216@qq.com>
2022-10-08 09:24:04 +0800

    idea first

---
+++ git-test/src/main/java/com/ztzz/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.ztzz.git;
+
+/**
+ * @author zd
+ * @create 2022-10-08 9:19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
